a1bd9c027743678d1ee4e6094410230a038b7bf2
Gabriel Franke <bannedboyinlife@gmail.com>
2022-01-31 19:29:01 -0300

    created script for ladder

--- DamnedForest/Assets/Scripts/Ladder.cs
+++ DamnedForest/Assets/Scripts/Ladder.cs
@@ -1,18 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class Ladder : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform playerController;
+    bool inside = false;
+    public float speed = 3f;
+    public FirstPersonController player;
+    public AudioSource sound;
+
     void Start()
     {
-        
+        player = GetComponent<FirstPersonController>();
+        inside = false;
     }
 
-    // Update is called once per frame
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Ladder")
+        {
+            Debug.Log("TouchingLadderTrue");
+            player.enabled = false;
+            inside = !inside;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Ladder")
+        {
+            Debug.Log("TouchingLadderFalse");
+            player.enabled = true;
+            inside = !inside;
+        }
+    }
+
     void Update()
     {
-        
+        if (inside == true && Input.GetKey("w"))
+        {
+            player.transform.position += Vector3.up /
+            speed * Time.deltaTime;
+        }
+
+        if (inside == true && Input.GetKey("s"))
+        {
+            player.transform.position += Vector3.down /
+            speed * Time.deltaTime;
+        }
+
+        if (inside == true && Input.GetKey("w") || inside == true && Input.GetKey("s"))
+        {
+            sound.enabled = true;
+            sound.loop = true;
+        }
+        else
+        {
+            sound.enabled = false;
+            sound.loop = false;
+        }
     }
 }
